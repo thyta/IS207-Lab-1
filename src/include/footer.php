3d6c21eb6a5986c8efff15a33bcdf1a2dfce7dd1
VDTK <21521039@gm.uit.edu.vn>
2023-10-31 21:19:32 +0700

    31/10

---
+++ src/include/footer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="Lab1.css">
+  <link rel="stylesheet" href="footer.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+    integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+     <!--Footer-->
+  <div class="container-fluid" style="height:2px; background-color: gainsboro;"></div>
+
+  <footer class="bg-white text-left text-black container">
+    <!-- Grid container -->
+    <div class="row footer-row">
+      <div class="col-3">
+        <h5  class="footer-brand " href="#">FURNIRO.</h5>
+        <p class="footer-uni"style="color:#9F9F9F">400 University Drive Suite 200 Coral Gables,
+          FL 33134 USA</p>
+      </div>
+      <div class="col-2">
+        <p style="color:#9F9F9F">Links</p>
+        <p>Home</p>
+        <p>Shop</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+      <div class="col-2">
+        <p style="color:#9F9F9F">Helps<p>
+        <p>Payment Options</p>
+        <p>Returns</p>
+        <p>Privacy Policies</p>
+      </div>
+      <div class="col">
+        <p style="color:#9F9F9F">Newsletters<p>
+  
+        <div class="row">
+          <div class="col-6">
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example20" class="form-control" style="border: none; border-bottom: 1px solid #000;" placeholder="Enter your email address" />
+            </div>
+            
+          </div>
+          <div class="col-4">
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example21" class="form-control" style="border: none; border-bottom: 1px solid #000;" placeholder="SUBCRIBE" />
+            </div>  
+          </div>
+  
+        </div>
+          
+        
+      </div>
+    </div>
+    <div class="container-fluid" style="height:2px; background-color: gainsboro;"></div>
+    <p style="padding: 20px;padding-left: 0;">2023 furino. All rights reverved</p>
+  </footer>
+</body>
+</html>
